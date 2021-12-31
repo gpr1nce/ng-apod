0ea41b89aac6581614eb962c94abe930382f4d5a
George Prince <prince.george35@gmail.com>
2021-12-31 10:07:06 -0600

    Minor edits for clarity

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ApodService } from '../api/apod.service';
+import { Apod } from '../models/apod';
 
 @Component({
   selector: 'app-apod',
   templateUrl: './apod.component.html',
   styleUrls: ['./apod.component.scss']
 })
+
 export class ApodComponent implements OnInit {
+
+  apod:Apod[] = [];
 
   constructor(private apodService: ApodService) { }
 
   ngOnInit() {
+    this.getApod();
+  }
+
+  getApod(): void{
     this.apodService.getApod().subscribe(
       (response:any)=>{
-        console.log(response);
+        this.apod = response;
       }
     );
   }
